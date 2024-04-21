ff1e73cb6c72b9794ec6efe29a4acb74128853b7
Teemu Kallio <Teemu.Kallio3@metropolia.fi>
2024-04-21 21:07:17 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Home from './components/Home';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 const App = () => {
+  const basePath = import.meta.env.BASE_URL;
+
   return (
-    <>
-      <h1>My App</h1>
-      <Home />
-    </>
+    <Router basename={basePath}>
+      <div>
+        <h1>My App</h1>
+        <Home />
+      </div>
+    </Router>
   );
 };
+
 export default App;
